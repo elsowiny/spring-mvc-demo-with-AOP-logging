b4d03d524cd87382036214ab0cb1a174cbfd9dfb
Sherief Elsowiny <elsowiny@yahoo.com>
2020-02-04 10:43:05 -0500

    spring-web-app

---
+++ src/com/elsowiny/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.elsowiny.springdemo.dao;
+
+import java.util.List;
+
+import com.elsowiny.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
